45b2a153b43f7c9ce9def188eca7a6e50016c26e
josuehernandez <joosuee.jv@gmail.com>
2013-03-25 00:38:58 -0600

    Create edad.js

---
+++ edad.js
@@ -0,0 +1,47 @@
+
+
+nombre = "josue";
+
+edad_josue = 16;
+
+mayor_edad = 18;
+  
+
+/* 
+
+if dice : lo que hay e n mis parentesis es verdad? 
+
+false = falso
+
+faso es verdad ? = flaso
+___________________________
+
+|| ó
+&& y aparte o solo "y"
+
+---------------------------
+LOGICOS
+
+< menor que 
+
+> mayor que
+
+== igual que
+
+!= difernte de
+
+<= menor ó igual
+
+>= mayor ó igual
+
+*/
+
+if( edad_josue >= 18   ){ 
+ 
+console.log("si es mayor")
+
+}else{
+
+console.log("no es mayor")
+
+}
